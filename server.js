ddd2b613aa4aa1dd112c9cc711eb0892c62473ec
Javad Gholizadeh <javad.gholizadeh@veritasadvisory.co.uk>
2022-04-26 18:46:58 +0100

    set up the server on node

--- server.js
+++ server.js
@@ -2,18 +2,41 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+const { response } = require('express');
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+
+const server = app.listen(port,listening);
+function listening(){
+    console.log(`running on localhost:${port}`);
+};
+
+
+// 1. add get route that returns projectData and post route 
+app.get('/all',sendData);
+
+function sendData(request,response){
+    response.send(projectData);
+};
+
+app.post('/add',addData){
+    projectData[""]
+}
